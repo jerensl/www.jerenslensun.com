45bc538ea24fa71736b45679b8075b7951fe214f
jerensl <jerenslensun@gmail.com>
2021-11-22 18:47:15 +0800

    fix frontpage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,24 +16,24 @@ const Home: NextPage = () => {
                 <Navbar />
                 <main className="min-h-9/10 w-full">
                     <div className="grid md:grid-cols-2 items-center justify-center pt-52 md:pt-40">
-                        <div className="relative w-full max-w-sm m-auto text-center pb-28 md:pb-16">
+                        <div className="relative w-full max-w-sm m-auto text-center pb-10 md:pb-16">
                             <h1 className="font-bold text-5xl">
                                 Jerens Lensun
                             </h1>
                             <p className="font-medium text-4xl">
                                 Software Engineer
                             </p>
-                            <div className="absolute top-0 -left-6 w-52 h-52 bg-purple-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob"></div>
-                            <div className="absolute top-0 -right-20 w-52 h-52 bg-yellow-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob animation-delay-2000"></div>
-                            <div className="absolute -bottom-10 left-28 w-52 h-52 bg-pink-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob animation-delay-4000"></div>
+                            <div className="absolute top-0 -left-6 w-40 h-40 md:w-52 md:h-52 bg-purple-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob"></div>
+                            <div className="absolute top-0 -right-10 md:-right-20 w-40 h-40 md:w-52 md:h-52 bg-yellow-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob animation-delay-2000"></div>
+                            <div className="absolute -bottom-10 left-20 md:left-28 w-40 h-40 md:w-52 md:h-52 bg-pink-300 blur-2xl rounded-full mix-blend-multiply filter opacity-70 animate-blob animation-delay-4000"></div>
                         </div>
                         <Image
                             loader={blobStorageIoImageLoader}
                             src="illustration-01.png"
                             alt="Person"
                             objectFit="contain"
-                            height="350px"
-                            width="180px"
+                            height="380px"
+                            width="170px"
                         />
                     </div>
                 </main>
