0163ae0ce2689e91ad53571fabd2fd94f02e71d8
jerensl <jerenslensun@gmail.com>
2021-11-22 08:58:57 +0800

    add config cdn for next/image

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,22 @@ export const Footer = (): React.ReactElement => {
     return (
         <footer className="grid justify-center h-1/10 place-items-center text-center shadow-inner dark:border-t-2 dark:border-gray-50 md:px-10 md:flex md:flex-row md:justify-between">
             <div className="flex space-x-5 py-1">
-                <a className="p-2" href="https://github.com/jerensl">
+                <a className="md:p-2" href="https://github.com/jerensl">
                     <FontAwesomeIcon
                         className="text-3xl transform hover:scale-125"
                         icon={['fab', 'github-square']}
                     />
                 </a>
-                <a className="p-2" href="https://twitter.com/jerensl22">
+                <a className="md:p-2" href="https://twitter.com/jerensl22">
                     <FontAwesomeIcon
                         className="text-3xl transform hover:scale-125"
                         icon={['fab', 'twitter-square']}
                     />
                 </a>
-                <a className="p-2" href="https://www.linkedin.com/in/jerensl/">
+                <a
+                    className="md:p-2"
+                    href="https://www.linkedin.com/in/jerensl/"
+                >
                     <FontAwesomeIcon
                         className="text-3xl transform	hover:scale-125"
                         icon={['fab', 'linkedin']}
