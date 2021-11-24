6ff8502f0f512a9d8ae37dc1c92529885c565a23
jerensl <jerenslensun@gmail.com>
2021-11-24 16:32:53 +0800

    adding image using CDN

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     swcMinify: true,
     images: {
-        loader: 'akamai',
+        loader: 'cloudinary',
         path: 'https://tempfilejerens.blob.core.windows.net/assets/illustration-01.png',
         domains: ['jerenslensun.com', 'tempfilejerens.blob.core.windows.net'],
     },
