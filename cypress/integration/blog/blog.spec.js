a88ab64ee68bc77a5ee1700b232dfc95099c61f3
jerensl <jerenslensun@gmail.com>
2021-12-11 21:14:55 +0800

    add e2e test using cypress & docker

---
+++ cypress/integration/blog/blog.spec.js
@@ -0,0 +1,18 @@
+describe('Blog page', () => {
+    it('should navigate to the article on blog page', () => {
+        // Start from the index page
+        cy.visit('/')
+
+        // Find a link with an href attribute containing "about" and click it
+        cy.get('a[href*="blog"]').click()
+
+        // Find a link with an href attribute containing "about" and click it
+        cy.contains('Read More →').click()
+
+        // The new url should include "/about"
+        cy.url().should('include', '/blog/fundamental-algoritma')
+
+        // The new page should contain an h1 with "About page"
+        cy.get('h3').contains('Growth Rate')
+    })
+})
