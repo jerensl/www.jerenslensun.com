3c83c69b81f96079c6e224b2638b3c8741f6341a
jerensl <jerenslensun@gmail.com>
2021-12-12 21:23:48 +0800

    ignore cypress screenshot and video

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,7 @@ export default function Custom404() {
         <>
             <Seo path="/" />
             <div className="min-h-full-screen">
-                <h1 className="pt-40 font-bold text-2xl text-center">
+                <h1 className="pt-40 font-bold text-3xl text-center">
                     404 - Page Not Found
                 </h1>
             </div>
