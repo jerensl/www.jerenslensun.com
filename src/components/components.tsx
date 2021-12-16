7c1ca6fe2e3b1c2c153a62f463a1ebfc8b327c0a
jerensl <jerenslensun@gmail.com>
2021-12-16 11:56:39 +0800

    changing syntax highlighting to back to stitces

--- src/components/components.tsx
+++ src/components/components.tsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import rangeParser from 'parse-numeric-range'
 import * as Collapsible from '@radix-ui/react-collapsible'
+import { pre } from './Pre'
 
 const components = {
-    pre: ({ children, theme }) => {
-        return <pre>{children}</pre>
+    pre: ({ children, theme, showLineNumbers, ...props }) => {
+        return (
+            <pre
+                className={pre({
+                    theme,
+                    showLineNumbers: typeof showLineNumbers === 'string',
+                    css: {
+                        mx: '-$4',
+                        mt: '$3',
+                        mb: '$5',
+
+                        '[data-preview] + &': {
+                            marginTop: '0',
+                            borderTopLeftRadius: 0,
+                            borderTopRightRadius: 0,
+                        },
+
+                        '@bp1': {
+                            mx: 0,
+                            borderRadius: '$3',
+                        },
+                    },
+                })}
+            >
+                {children}
+            </pre>
+        )
     },
     code: ({ children, id, collapsible }) => {
         const isCollapsible = typeof collapsible !== 'undefined'
