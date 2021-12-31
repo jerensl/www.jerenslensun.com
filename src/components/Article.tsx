d9c5e1769e54a7ad472ab71ef633cb04e3e952dd
jerensl <jerenslensun@gmail.com>
2021-12-31 18:37:26 +0800

    add unit test to improve coverage

---
+++ src/components/Article.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from 'react'
+import { getMDXComponent } from 'mdx-bundler/client'
+import Image from 'next/image'
+import { ArticleSeo } from './Seo'
+import { components } from './components'
+
+const blobStorageIoImageLoader = ({ src }) => {
+    return `https://res.cloudinary.com/do9os7lxv/image/upload/v1637714730/personal/${src}`
+}
+
+export const Article = ({ frontmatter, code }): React.ReactElement => {
+    const Component = useMemo(() => getMDXComponent(code), [code])
+
+    return (
+        <>
+            <ArticleSeo
+                path={frontmatter.title}
+                title={frontmatter.title}
+                description={frontmatter.summary}
+            />
+
+            <header className="pt-20 text-center">
+                <h1 className="text-3xl font-bold">{frontmatter.title}</h1>
+                <p className="text-xl pt-1 pb-2 font-medium">
+                    {frontmatter.date}
+                </p>
+                <Image
+                    loader={blobStorageIoImageLoader}
+                    src={frontmatter.cover}
+                    alt="Person"
+                    objectFit="cover"
+                    height="350px"
+                    width="900px"
+                />
+            </header>
+            <main className="min-h-9/10 pt-2 md:pt-10 font-medium px-4 md:px-10">
+                <article className="prose prose-p:font-normal prose-li:font-normal max-w-4xl lg:prose-lg m-auto">
+                    <Component components={components} />
+                </article>
+            </main>
+        </>
+    )
+}
--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -6,17 +6,9 @@ import {
 } from 'next'
 import { getListOfArticle } from '../../domain/Blog'
 import { getArticleWithMetadata } from '../../domain/Article'
-import { getMDXComponent } from 'mdx-bundler/client'
-import { useMemo } from 'react'
 import { Navbar } from '../../components/Navbar'
-import { components } from '../../components/components'
 import { Footer } from '../../components/Footer'
-import { ArticleSeo } from '../../components/Seo'
-import Image from 'next/image'
-
-const blobStorageIoImageLoader = ({ src }) => {
-    return `https://res.cloudinary.com/do9os7lxv/image/upload/v1637714730/personal/${src}`
-}
+import { Article } from '../../components/Article'
 
 export const getStaticPaths: GetStaticPaths = async () => {
     const posts = getListOfArticle('contents')
@@ -46,35 +38,10 @@ export default function Blog({
 }: InferGetStaticPropsType<typeof getStaticProps>): React.ReactElement {
     const { code, frontmatter } = posts
 
-    const Component = useMemo(() => getMDXComponent(code), [code])
-
     return (
         <>
-            <ArticleSeo
-                path={frontmatter.title}
-                title={frontmatter.title}
-                description={frontmatter.summary}
-            />
             <Navbar />
-            <header className="pt-20 text-center">
-                <h1 className="text-3xl font-bold">{frontmatter.title}</h1>
-                <p className="text-xl pt-1 pb-2 font-medium">
-                    {frontmatter.date}
-                </p>
-                <Image
-                    loader={blobStorageIoImageLoader}
-                    src={frontmatter.cover}
-                    alt="Person"
-                    objectFit="cover"
-                    height="350px"
-                    width="900px"
-                />
-            </header>
-            <main className="min-h-9/10 pt-2 md:pt-10 font-medium px-4 md:px-10">
-                <article className="prose prose-p:font-normal prose-li:font-normal max-w-4xl lg:prose-lg m-auto">
-                    <Component components={components} />
-                </article>
-            </main>
+            <Article frontmatter={frontmatter} code={code} />
             <Footer />
         </>
     )
