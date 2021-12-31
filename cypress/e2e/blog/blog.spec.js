9f94362ea8d0a1730c9cd0ff05085e3a4d7374f4
jerensl <jerenslensun@gmail.com>
2021-12-31 18:50:59 +0800

    fix e2e test click

--- cypress/e2e/blog/blog.spec.js
+++ cypress/e2e/blog/blog.spec.js
@@ -1,13 +1,15 @@
 describe('Blog page', () => {
     it('should navigate to the article on blog page', () => {
         // Start from the index page
-        cy.visit('/')
+        cy.visit('/blog')
 
         // Find a link with an href attribute containing article "self-reflection-2021" and click it
         cy.get('a[href*="blog"]').click()
 
         // Find a link with an href attribute containing article "self-reflection-2021" and click it
-        cy.get('a[href*="/blog/self-reflection-2021"]').click()
+        cy.get('a[href*="/blog/self-reflection-2021"]').click({
+            multiple: true,
+        })
 
         // The new url should have text "Be Present"
         cy.get('strong').contains('Be Present')
