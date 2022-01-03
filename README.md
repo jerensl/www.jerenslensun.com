d4308da8d7c82daf502a9476f44a1ab279a75f8f
jerensl <jerenslensun@gmail.com>
2022-01-03 22:46:28 +0800

    change logo and icons

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### Personal Web App
+### jerenslensun.com
 
 [![codecov](https://codecov.io/gh/jerensl/personal-web-app/branch/main/graph/badge.svg?token=HSYPO9VBAU)](https://codecov.io/gh/jerensl/personal-web-app) ![Continuous Integration](https://github.com/jerensl/personal-web-app/actions/workflows/ci.yml/badge.svg) ![Continuous Deployment](https://github.com/jerensl/personal-web-app/actions/workflows/cd.yml/badge.svg)
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,14 +42,83 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                     <meta name="description" content={APP_DESCRIPTION} />
                     <meta name="format-detection" content="telephone=no" />
                     <meta name="mobile-web-app-capable" content="yes" />
-                    <meta name="theme-color" content="#FFFFFF" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="57x57"
+                        href="/icons/apple-icon-57x57.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="60x60"
+                        href="/icons/apple-icon-60x60.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="72x72"
+                        href="/icons/apple-icon-72x72.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="76x76"
+                        href="/icons/apple-icon-76x76.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="114x114"
+                        href="/icons/apple-icon-114x114.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="120x120"
+                        href="/icons/apple-icon-120x120.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="144x144"
+                        href="/icons/apple-icon-144x144.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="152x152"
+                        href="/icons/apple-icon-152x152.png"
+                    />
                     <link
                         rel="apple-touch-icon"
                         sizes="180x180"
-                        href="/icons/apple-touch-icon.png"
+                        href="/icons/apple-icon-180x180.png"
                     />
-                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="192x192"
+                        href="/icons/android-icon-192x192.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="32x32"
+                        href="/icons/favicon-32x32.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="96x96"
+                        href="/icons/favicon-96x96.png"
+                    />
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="16x16"
+                        href="/icons/favicon-16x16.png"
+                    />
                     <link rel="manifest" href="/manifest.json" />
+                    <meta name="msapplication-TileColor" content="#ffffff" />
+                    <meta
+                        name="msapplication-TileImage"
+                        content="/icons/ms-icon-144x144.png"
+                    />
+                    <meta name="theme-color" content="#FFFFFF" />
                     <link
                         rel="stylesheet"
                         href="https://cdn.jsdelivr.net/npm/katex@0.13.13/dist/katex.min.css"
