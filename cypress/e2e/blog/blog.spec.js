da5b9eed11b391682738985070f2305b869155a1
jerensl <jerenslensun@gmail.com>
2022-01-08 21:27:51 +0800

    fix e2e when changing style landing page and blog page

--- cypress/e2e/blog/blog.spec.js
+++ cypress/e2e/blog/blog.spec.js
@@ -4,12 +4,10 @@ describe('Blog page', () => {
         cy.visit('/')
 
         // Find a link with an href attribute containing article "self-reflection-2021" and click it
-        cy.get('a[href*="blog"]').click()
+        cy.get('a[href*="blog"]').first().click()
 
         // Find a link with an href attribute containing article "self-reflection-2021" and click it
-        cy.get('a[href*="/blog/self-reflection-2021"]').click({
-            multiple: true,
-        })
+        cy.get('a[href*="/blog/self-reflection-2021"]').first().click()
 
         // The new url should have text "Be Present"
         cy.get('strong').contains('Be Present')
