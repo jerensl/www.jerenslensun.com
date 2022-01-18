f8dbfbafad118085142e600b7b6aa7dbc38b700c
jerensl <jerenslensun@gmail.com>
2022-01-18 21:55:18 +0800

    adding feature tag to blog

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,31 +1,40 @@
 import { GetStaticProps, InferGetStaticPropsType } from 'next'
-import { getAllPublishArticle, sortByLatestDate, Metadata } from '@/domain/Blog'
+import {
+    getAllPublishArticle,
+    sortByLatestDate,
+    Metadata,
+    getAllTags,
+} from '@/domain/Blog'
 import { Navbar } from '@/components/Navbar'
 import { Footer } from '@/components/Footer'
 import { Seo } from '@/components/Seo'
 import { SearchArticle } from '@/components/SearchArticle'
+import { Tag } from '@/components/Tag'
 
 export const getStaticProps: GetStaticProps = async () => {
     const posts: Array<Metadata> = await getAllPublishArticle(
         'contents',
         sortByLatestDate
     )
+    const tags = getAllTags(posts)
 
     return {
-        props: { posts },
+        props: { posts, tags },
     }
 }
 
 export default function Blog({
     posts,
+    tags,
 }: InferGetStaticPropsType<typeof getStaticProps>): React.ReactElement {
     return (
         <>
             <Seo path="/blog" />
             <Navbar />
             <main className="min-h-9/10 flex px-4 md:px-10 flex-col gap-5">
-                <h1 className="pt-20 text-2xl">Blog</h1>
-                <SearchArticle posts={posts} />
+                <h1 className="pt-20 text-2xl font-semibold">Blog</h1>
+
+                <SearchArticle posts={posts} tags={tags} />
             </main>
             <Footer />
         </>
