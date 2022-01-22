fa24c394b9e63eed169e980aa182e2f04e2ec7bc
jerensl <jerenslensun@gmail.com>
2022-01-22 12:22:27 +0800

    fix test e2e

--- cypress/e2e/blog/blog.spec.js
+++ cypress/e2e/blog/blog.spec.js
@@ -2,15 +2,14 @@ describe('Blog page', () => {
     it('should navigate to the article on blog page', () => {
         // Start from the index page
         cy.visit('/')
-        const articleTitle = 'Self Reflection 2021'
 
         // Find a link with an href attribute containing blog page
-        cy.get('a[href*="blog"]').first().click()
+        cy.get('a[href*="blog"').first().click()
 
         cy.get('[data-cy=lang]').click()
 
         // Find a link with an href attribute containing article "self-reflection-2021" and click it
-        cy.get('h1').should('have.value', articleTitle).click()
+        cy.get('a[href*="self-reflection-2021').click()
 
         // The new url should have text "Be Present"
         cy.get('strong').contains('Be Present')
@@ -20,8 +19,7 @@ describe('Blog page', () => {
         // Start from the index page
         cy.visit('/')
 
-        const typedText = 'Algoritma'
-        const articleTitle = 'fundamentalalgoritma'
+        const typedText = 'Fundamental Algoritma'
 
         // Find a link with an href attribute containing blog page
         cy.get('a[href*="blog"]').first().click()
@@ -33,7 +31,7 @@ describe('Blog page', () => {
             .should('have.value', typedText)
 
         // Find a link with an href attribute containing article "fundamental-algoritma" and click it
-        cy.get('h1').should('have.value', articleTitle).click()
+        cy.get('a[href*="fundamental-algoritma"').click()
 
         // The new url should have text "Be Present"
         cy.get('h3').contains('Growth Rate')
