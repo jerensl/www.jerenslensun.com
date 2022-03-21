0a97a6373fbb61eb492e8d61ab2860f0fbeea45e
jerensl <jerenslensun@gmail.com>
2022-03-21 14:36:25 +0800

    adding feature notification

--- src/lib/firebase-init.ts
+++ src/lib/firebase-init.ts
@@ -29,7 +29,22 @@ export const getCredentialsToken = async () => {
     try {
         currentToken = await getToken(messaging, { vapidKey: FCM_VAPID_KEY })
         if (currentToken) {
-            console.log(currentToken)
+            const response = await fetch(
+                'http://api.jerenslensun.com/api/notification/subscribe',
+                {
+                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                    mode: 'cors', // no-cors, *cors, same-origin
+                    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                    credentials: 'same-origin', // include, *same-origin, omit
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        token: currentToken,
+                    }), // body data type must match "Content-Type" header
+                }
+            )
+            console.log(response.json)
         } else {
             console.log(currentToken)
         }
