40b8a21021fee18b4c4a3b96f663a25027db745d
Jerens Lensun <54782057+jerensl@users.noreply.github.com>
2022-03-24 18:06:28 +0800

    fix double notification

--- src/components/Notifications.tsx
+++ src/components/Notifications.tsx
@@ -5,7 +5,10 @@ import { firebaseApp } from '../lib/firebase-init'
 
 export const Notifications = (): React.ReactElement => {
     useEffect(() => {
-        if (window.Notification.permission === 'granted') {
+        if (
+            'Notification' in window &&
+            window.Notification.permission === 'granted'
+        ) {
             const messaging = getMessaging(firebaseApp)
 
             onMessage(messaging, (payload) => {
