4981d24055e711a8cd936d5706dc92ff20e51e85
Jerens Lensun <54782057+jerensl@users.noreply.github.com>
2022-03-26 20:32:08 +0800

    fix notification doest work

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,23 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js')
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js')
+import { initializeApp } from 'firebase/app'
+import { getMessaging } from 'firebase/messaging/sw'
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyAlgD74xXwIcDLkdju_zeB9ntqCiGN5xko',
-    authDomain: 'jerens-app.firebaseapp.com',
-    projectId: 'jerens-app',
-    storageBucket: 'jerens-app.appspot.com',
-    messagingSenderId: '1009489116025',
-    appId: '1:1009489116025:web:c64b30012bbc6a7f274da8',
-    measurementId: 'G-SNRQJJZH3L',
-}
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const firebaseApp = initializeApp({
+    apiKey: 'api-key',
+    authDomain: 'project-id.firebaseapp.com',
+    databaseURL: 'https://project-id.firebaseio.com',
+    projectId: 'project-id',
+    storageBucket: 'project-id.appspot.com',
+    messagingSenderId: 'sender-id',
+    appId: 'app-id',
+    measurementId: 'G-measurement-id',
+})
 
-firebase.initializeApp(firebaseConfig)
-
-const messaging = firebase.messaging()
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = getMessaging(firebaseApp)
 
 onBackgroundMessage(messaging, (payload) => {
     const notificationTitle = payload.notification.title
