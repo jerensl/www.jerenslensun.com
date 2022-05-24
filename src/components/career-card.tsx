f01de43bac067b47aadd5c998f82e4f785434069
jerensl <54782057+jerensl@users.noreply.github.com>
2022-05-24 14:39:00 +0800

    refactor: adding animation on carrer section

--- src/components/career-card.tsx
+++ src/components/career-card.tsx
@@ -1,20 +1,41 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { format } from 'date-fns'
+import { useReducedMotion, motion } from 'framer-motion'
 
 export const CareerCard = ({
     organization,
     date,
     role,
+    inView,
 }): React.ReactElement => {
+    const shouldReduceMotion = useReducedMotion()
+
+    const childrenVariants = {
+        initial: { opacity: 0, y: shouldReduceMotion ? 0 : 25 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    }
+
     return (
-        <div
-            key={organization}
+        <motion.div
             className="col-span-full md:col-span-4 lg:col-span-3 p-2 "
+            initial="initial"
+            key={organization}
+            animate={inView ? 'visible' : 'hidden'}
+            variants={{
+                initial: { opacity: 0 },
+                visible: {
+                    opacity: 1,
+                    transition: { staggerChildren: 0.2 },
+                },
+            }}
         >
-            <h3 className="text-xl font-bold">{organization}</h3>
-            <p>{role}</p>
-            <span className="font-light pt-2">{date}</span>
-        </div>
+            <motion.div variants={childrenVariants}>
+                <h3 className="text-xl font-bold">{organization}</h3>
+            </motion.div>
+            <motion.div variants={childrenVariants}>
+                <p>{role}</p>
+            </motion.div>
+            <motion.div variants={childrenVariants}>
+                <span className="font-light pt-2">{date}</span>
+            </motion.div>
+        </motion.div>
     )
 }
--- src/components/sections/career-section.tsx
+++ src/components/sections/career-section.tsx
@@ -4,37 +4,73 @@ import clsx from 'clsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFile } from '@fortawesome/free-solid-svg-icons'
 import { CareerCard } from '../career-card'
+import { InView } from 'react-intersection-observer'
+import { useReducedMotion, motion } from 'framer-motion'
 
 export const CareerSection = (): React.ReactElement => {
-    return (
-        <Grid className="gap-5 lg:gap-8">
-            <div className="col-span-full lg:col-span-9">
-                <h1 className="text-5xl font-bold text-center md:text-left">
-                    Career
-                </h1>
-            </div>
-            <div className="col-span-full lg:col-span-3">
-                <a
-                    href="https://drive.google.com/file/d/17cKewjUgjQomJ32VMxaWo1oigN-v1eqe/view?usp=sharing"
-                    target="_blank"
-                    className="inline-flex font-semibold items-center gap-3 text-2xl py-2 px-4 rounded-lg border-b-2 transition duration-150 ease-in-out hover:bg-gray-50 border-gray-100 hover:border-gray-300"
-                    rel="noopener noreferrer"
-                >
-                    <span>Get My CV</span>
-                    <FontAwesomeIcon className="block" icon={faFile} />
-                </a>
-            </div>
-            <CareerCard
-                organization="Ministry of Tourism"
-                role="Facilitator"
-                date="October 2020 – November 2020"
-            />
+    const shouldReduceMotion = useReducedMotion()
 
-            <CareerCard
-                organization="Bangkit Academy 2022"
-                role="Facilitator"
-                date="February 2022 – Present"
-            />
-        </Grid>
+    const childrenVariants = {
+        initial: { opacity: 0, y: shouldReduceMotion ? 0 : 25 },
+        visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    }
+
+    return (
+        <InView triggerOnce rootMargin="-40% 0px">
+            {({ inView, ref }) => (
+                <Grid ref={ref} className="gap-5 lg:gap-8">
+                    <motion.div
+                        className="col-span-full lg:col-span-9"
+                        initial="initial"
+                        animate={inView ? 'visible' : 'hidden'}
+                        variants={{
+                            initial: { opacity: 0 },
+                            visible: {
+                                opacity: 1,
+                                transition: { staggerChildren: 0.2 },
+                            },
+                        }}
+                    >
+                        <h1 className="text-5xl font-bold text-center md:text-left">
+                            Career
+                        </h1>
+                    </motion.div>
+                    <motion.div
+                        className="col-span-full lg:col-span-3"
+                        initial="initial"
+                        animate={inView ? 'visible' : 'hidden'}
+                        variants={{
+                            initial: { opacity: 0 },
+                            visible: {
+                                opacity: 1,
+                                transition: { staggerChildren: 0.2 },
+                            },
+                        }}
+                    >
+                        <a
+                            href="https://drive.google.com/file/d/17cKewjUgjQomJ32VMxaWo1oigN-v1eqe/view?usp=sharing"
+                            target="_blank"
+                            className="inline-flex font-semibold items-center gap-3 text-2xl py-2 px-4 rounded-lg border-b-2 transition duration-150 ease-in-out hover:bg-gray-50 border-gray-100 hover:border-gray-300"
+                            rel="noopener noreferrer"
+                        >
+                            <span>Get My CV</span>
+                            <FontAwesomeIcon className="block" icon={faFile} />
+                        </a>
+                    </motion.div>
+                    <CareerCard
+                        inView={inView}
+                        organization="Ministry of Tourism"
+                        role="Facilitator Baparekraf Digital Talent"
+                        date="October 2020 – November 2020"
+                    />
+                    <CareerCard
+                        inView={inView}
+                        organization="Bangkit Academy 2022"
+                        role="Facilitator"
+                        date="February 2022 – Present"
+                    />
+                </Grid>
+            )}
+        </InView>
     )
 }
