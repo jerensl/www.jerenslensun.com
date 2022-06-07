07ba92bc536275614fb261bd5dc9ccac6e0fa247
jerensl <54782057+jerensl@users.noreply.github.com>
2022-06-07 18:54:24 +0800

    fix: fix cypress adress not valid

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,11 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
     e2e: {
+        baseUrl: 'http://localhost:3000',
         projectId: 'qwg648',
-        baseUrl: 'http://localhost:1234',
         specPattern: 'cypress/e2e',
         experimentalFetchPolyfill: true,
         supportFile: 'cypress/support/e2e.{js,jsx,ts,tsx}',
-        setupNodeEvents(on, config) {
-            // e2e testing node events setup code
-        },
+        setupNodeEvents(on, config) {},
     },
 })
