e38349f1124c9d84e7f326a3322379abe15b32e8
jerensl <54782057+jerensl@users.noreply.github.com>
2022-07-09 19:57:25 +0800

    fix: fix gtm not working across pages

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 interface SiteMetadata {
     url: string
@@ -41,22 +42,42 @@ export const ArticleSeo = ({
     image,
 }: BlogPostSeoProps) => {
     return (
-        <Head>
-            <title>{`${title}`}</title>
-            <meta name="robots" content="follow, index" />
-            <meta name="description" content={description} />
-            <meta property="og:url" content={`${siteMetadata.url}${path}`} />
-            <meta property="og:title" content={title} />
-            <meta property="og:description" content={description} />
-            <meta property="og:site_name" content={app.name} />
-            <meta property="og:type" content="website" />
-            <meta name="image" property="og:image" content={image} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content={siteMetadata.twitter} />
-            <meta name="twitter:title" content={title} />
-            <meta name="twitter:description" content={description} />
-            <meta name="twitter:image" content={image} />
-        </Head>
+        <>
+            <Head>
+                <title>{`${title}`}</title>
+                <meta name="robots" content="follow, index" />
+                <meta name="description" content={description} />
+                <meta
+                    property="og:url"
+                    content={`${siteMetadata.url}${path}`}
+                />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:site_name" content={app.name} />
+                <meta property="og:type" content="website" />
+                <meta name="image" property="og:image" content={image} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content={siteMetadata.twitter} />
+                <meta name="twitter:title" content={title} />
+                <meta name="twitter:description" content={description} />
+                <meta name="twitter:image" content={image} />
+            </Head>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=GTM-PKW49NT"
+                strategy="afterInteractive"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PKW49NT');`,
+                }}
+            />
+        </>
     )
 }
 
@@ -66,22 +87,42 @@ interface PageSeo {
 
 export const Seo = ({ path }: PageSeo) => {
     return (
-        <Head>
-            <title>{`${app.title}`}</title>
-            <meta name="robots" content="follow, index" />
-            <meta name="description" content={app.description} />
-            <meta property="og:url" content={`${siteMetadata.url}${path}`} />
-            <meta property="og:title" content={app.title} />
-            <meta property="og:description" content={app.description} />
-            <meta property="og:site_name" content={app.name} />
-            <meta property="og:type" content="website" />
-            <meta name="image" property="og:image" content={app.image} />
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:site" content={siteMetadata.twitter} />
-            <meta name="twitter:title" content={app.title} />
-            <meta name="twitter:description" content={app.description} />
-            <meta name="twitter:image" content={app.image} />
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
+        <>
+            <Head>
+                <title>{`${app.title}`}</title>
+                <meta name="robots" content="follow, index" />
+                <meta name="description" content={app.description} />
+                <meta
+                    property="og:url"
+                    content={`${siteMetadata.url}${path}`}
+                />
+                <meta property="og:title" content={app.title} />
+                <meta property="og:description" content={app.description} />
+                <meta property="og:site_name" content={app.name} />
+                <meta property="og:type" content="website" />
+                <meta name="image" property="og:image" content={app.image} />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content={siteMetadata.twitter} />
+                <meta name="twitter:title" content={app.title} />
+                <meta name="twitter:description" content={app.description} />
+                <meta name="twitter:image" content={app.image} />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=GTM-PKW49NT"
+                strategy="afterInteractive"
+            />
+            <Script
+                id="google-analytics"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PKW49NT');`,
+                }}
+            />
+        </>
     )
 }
