32570ac076e21573ec51945d9fd12998f004530f
Jerens Lensun <jerensslensun@gmail.com>
2026-02-02 20:14:47 +0800

    fix: azure deployment and playwright e2e test
    Signed-off-by: Jerens Lensun <jerensslensun@gmail.com>

--- apps/web-app/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6rsnVz5oyuHJme0DaOyR4gtrCDjEPdGiZwm0JL5HEOw=\"\n}"
--- apps/web-app/.next/dev/static/chunks/src_mocks_init_ts_352efb77._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_mocks_browser_ts_1aeaf92a._.js",
-  "static/chunks/src_mocks_init_ts_2708ad80._.js"
-],
-    source: "dynamic"
-});
